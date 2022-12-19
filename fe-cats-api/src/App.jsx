f680de653184f407c1cd979099441d0081a76014
ravaattar <rava.naufal2014@gmail.com>
2022-12-20 00:01:41 +0700

    init vite + tailwindcss project

---
+++ fe-cats-api/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-5xl font-bold text-cyan-400">
+        Hello, world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
